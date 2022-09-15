22d0dfbefe181063928f52c1bfc2a5314dc75b47
Cam <cameron.miller294@gmail.com>
2022-09-14 20:46:42 -0400

    Update style.css
    Updated sub-window layout of UI sidebar to match wireframe

---
+++ public/js/themes.js
@@ -0,0 +1,15 @@
+const themes = [
+  {
+    title: 'Skel',
+    textColor: '606060',
+    backgroundColor: 'FFFFFF',
+    selectedColor: 'D6D6D6'
+  },
+  {
+    title: 'Dark'
+
+  },
+  {
+    title: 'Pastel'
+  }
+];
