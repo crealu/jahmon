8e306bba52204bf361738792fe441071ca95a4a6
Cam <cameron.miller294@gmail.com>
2022-09-16 19:41:15 -0400

    Updated lib.js
    Rewrote list.js route new data string manipulation

--- routes/lib.js
+++ routes/lib.js
@@ -4,11 +4,20 @@ const fs = require('fs');
 const libraryData = require('../library.json');
 
 function reStringLib(str) {
-  return str.replace('}]}', '}').replace(/{"n/g, '\n\t{"n');
+  return str
+    .replace('}]}', '}')
+    .replace(/{"n/g, '\n\t\t{\n\t\t\t"n')
+    .replace(/"no/g, '\n\t\t\t"no')
+    .replace(/:/g, ': ')
+    .replace(/},/g, '\n\t\t},')
 }
 
-function reString(str) {
-  return str.replace('{', ',\n\t{')
+function reStringNew(str) {
+  return str
+    .replace('{', ',\n\t\t{\n\t\t\t')
+    .replace(/"type":"chord",/g, '\n\t\t\t')
+    .replace(/:/g, ': ')
+    .replace('}', '\n\t\t}')
 }
 
 function getJsonEnd() {
@@ -18,7 +27,7 @@ function getJsonEnd() {
 router.post('/lib', (req, res) => {
   console.log(req.body);
   let libData = reStringLib(JSON.stringify(libraryData));
-  let newData = reString(JSON.stringify(req.body));
+  let newData = reStringNew(JSON.stringify(req.body));
   let jsonEnd = getJsonEnd();
   let data = libData + newData + jsonEnd;
   fs.writeFile('library.json', data, (err) => {
