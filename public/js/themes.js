f93fae83a6fbaf8ca3ed922fb5ca92610006a8aa
Cam <cameron.miller294@gmail.com>
2022-09-18 20:19:39 -0400

    Updated theme.js
    Added a basic theme change implementation

--- public/js/themes.js
+++ public/js/themes.js
@@ -1,15 +1,64 @@
 const themes = [
   {
-    title: 'Skel',
-    textColor: '606060',
-    backgroundColor: 'FFFFFF',
-    selectedColor: 'D6D6D6'
+    name: 'skel',
+    sequence: {
+      textColor: '606060',
+      backgroundColor: 'FFFFFF',
+      selectedColor: 'D6D6D6'
+    }
   },
   {
-    title: 'Dark'
-
+    name: 'dark',
+    sequence: {
+      textColor: 'E2E2E2',
+      backgroundColor: '3F3F3F',
+      selectedColor: '1E1E1E'
+    }
   },
   {
-    title: 'Pastel'
+    name: 'pastel'
   }
 ];
+
+// const themes = {
+//   skel: {
+//     changes: [
+//       {
+//         element: 'saved-sequences',
+//         color: ''
+//       }
+//     ]
+//     sequence: {
+//       textColor: '606060',
+//       backgroundColor: 'FFFFFF',
+//       selectedColor: 'D6D6D6'
+//     }
+//   },
+//   dark: {
+//     sequence: {
+//       textColor: 'E2E2E2',
+//       backgroundColor: '3F3F3F',
+//       selectedColor: '1E1E1E'
+//     }
+//   },
+//   pastel: {
+//     sequence: {
+//       textColor: '606060',
+//       backgroundColor: 'FFFFFF',
+//       selectedColor: 'D6D6D6'
+//     }
+//   }
+// }
+
+const themeSelect = document.getElementsByClassName('theme-select')[0];
+
+function changeTheme(option) {
+  for (theme in themes) {
+
+    console.log(themes[theme]);
+  }
+}
+
+themeSelect.addEventListener('change', (e) => {
+  changeTheme(e.target.value)
+});
