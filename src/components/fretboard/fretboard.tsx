e19c4e6a352cb81bae4e0ca123f8322805094f08
Cam <cameron.miller294@gmail.com>
2022-10-31 20:41:45 -0400

    Update fretboard UI
    Added fretboard UI components to Fretboard React component

--- src/components/fretboard/fretboard.tsx
+++ src/components/fretboard/fretboard.tsx
@@ -8,16 +8,12 @@ const TheFretboard = () => {
   const returnFrets = (sn) => {
     return (
       <div className="string-row">
-        <div class="string-div"></div>
-        <div class="fret-open" data-noteid={`s${sn + 1}f0`}></div>
+        <div className="string-div"></div>
+        <div className="fret-open" data-noteid={`s${sn + 1}f0`}></div>
         {frets.map((fret, fn) => {
           return (
             <div className="fret">
-              <div
-                className="fret-note"
-                data-noteid={`s${sn}f${fn}`}
-              >
-              </div>
+              <div className="fret-note" data-noteid={`s${sn}f${fn}`}></div>
             </div>
           )
         })}
@@ -38,10 +34,19 @@ export const Fretboard: React.FC = (): React.ReactElement => {
       <div className="fretboard-top">
         <h3 className="section-title">Fretboard</h3>
         <div className="sequence-btn-wrapper">
-          <img className="sequence-btn delete-btn" src="img/icons/delete-btn-gray.png"/>
+          <img className="sequence-btn add-btn" src="img/icons/add-btn-gray.png"/>
           <img className="sequence-btn" src="img/icons/clear-btn-gray.png"/>
           <img className="sequence-btn" src="img/icons/save-btn-gray.png"/>
         </div>
+        <div className="mode-wrapper">
+          <p className="mode-title mode-toggle">Mode</p>
+          <img className="mode-trail mode-toggle" src="img/icons/mode-trail-chord.png"/>
+          <div className="mode-type-wrapper mode-toggle">
+            <div className="mode-toggle-btn chord-btn" onClick="">Chord</div>
+            <div className="mode-toggle-btn riff-btn">Riff</div>
+          </div>
+        </div>
+        <div className="seq-step-name"></div>
       </div>
       <TheFretboard />
     </div>
--- src/components/sequence/sequence.tsx
+++ src/components/sequence/sequence.tsx
@@ -8,10 +8,7 @@ const SequenceTitle = () => {
   const title = useAppSelector(currentTitle);
 
   return (
-    <div
-      className="section-title"
-      data-isnew="new"
-    >
+    <div className="section-title" data-isnew="new">
       {title == '' ? 'untitled' : title}
     </div>
   )
@@ -20,9 +17,92 @@ const SequenceTitle = () => {
 export const Sequence: React.FC = (): React.ReactElement => {
   const seq = useAppSelector(currentSeq);
   const [steps, setSteps] = useState(seq);
+  const [activeStep, setActiveStep] = useState(0);
 
-  const printSteps = () => {
-    console.log(seq);
+  function updateActiveStep(step, propers, idx) {
+    console.log(step);
+    let steps = document.getElementsByClassName('seq-step');
+    for (let s = 0; s < steps.length; s++) {
+      steps[s].style.background = 'none';
+    }
+    step.style.background = 'white';
+    clearFretboard();
+    populateFretboard(step, propers);
+    setActiveStep(step.dataset.stepnum);
+    // activeStep = step.dataset.stepnum;
+  }
+
+  function clearFretboard() {
+    const noteBubbles = document.getElementsByClassName('note-bubble-fret');
+    const fretNotes = document.getElementsByClassName('fret-note');
+
+    for (let fn = 0; fn < fretNotes.length; fn++) {
+      fretNotes[fn].style.display = 'none';
+    }
+
+    for (let nb = 0; nb < noteBubbles.length; nb++) {
+      noteBubbles[nb].style.display = 'none';
+    }
+
+    for (let n = 0; n < noteBubbles.length; n++) {
+      noteBubbles[n].remove();
+    }
+  }
+
+  function populateFretboard(step, propers) {
+    toggleMode(step.data.mode);
+    const noteIds = propers.noteids.split(',');
+    let fretnums, trueFretnums;
+
+    if (propers.mode == 'riff') {
+      fretnums = propers.fretnums.split(',');
+      trueFretnums = fretnums.filter((fretnum) => fretnum != '');
+    }
+
+    placeNotes(step, noteIds, trueFretnums);
+  }
+
+  function placeNotes(step, noteIds, trueFretnums) {
+    const fretNotes = document.getElementsByClassName('fret-note');
+    console.log(noteIds, fretNotes)
+    for (let n = 0; n < noteIds.length; n++) {
+      for (let fn = 0; fn < fretNotes.length; fn++) {
+        if (noteIds[n] == fretNotes[fn].dataset.noteid) {
+          console.log(noteIds[n], fretNotes[fn]);
+          // console.log(fretNotes[fn].data.noteid)
+          // if (step.dataset.mode == 'chord') {
+            fretNotes[fn].style.display = 'block';
+          // }
+        }
+      }
+    }
+  }
+
+  function toggleMode(newMode) {
+    let riffNumbers = document.getElementsByClassName('riff-numbers')[0];
+    if (newMode == 'riff') {
+      setModeStyle('rgba(0, 0, 0, 0)', '0');
+      riffNumbers.style.opacity = '1';
+      chordBtn.style.background = 'var(--skel_sel_color)';
+      riffBtn.style.background = 'var(--skel_bg_color)';
+    } else if (newMode == 'chord') {
+      setModeStyle('lightgray', '1');
+      riffNumbers.style.opacity = '0';
+      chordBtn.style.background = 'var(--skel_bg_color)';
+      riffBtn.style.background = 'var(--skel_sel_color)';
+    }
+    mode = newMode;
+  }
+
+  function setModeStyle(fretBorder, circleOpacity) {
+    let frets = document.getElementsByClassName('fret');
+    let fretCircles = document.getElementsByClassName('fret-circle');
+    for (let f = 0; f < frets.length; f++) {
+      frets[f].style.borderLeft = '1px solid ' + fretBorder;
+    }
+    for (let fc = 0; fc < fretCircles.length; fc++) {
+      fretCircles[fc].style.opacity = circleOpacity;
+    }
   }
 
   return (
@@ -36,18 +116,20 @@ export const Sequence: React.FC = (): React.ReactElement => {
         </div>
       </div>
       <div className="steps-wrapper">
-      {seq.map(step => {
-        return (
-            <div
-              className="seq-step"
-              dataset-noteids={step.noteids}
-              dataset-modes={step.modes}
-              dataset-fretnums={step.fretnums}
-            >
-              {step.title}
-            </div>
-          )
-      })}
+        {seq.map((step, i) => {
+          return (
+              <div
+                className="seq-step"
+                onClick={(e) => { updateActiveStep(e.target, {
+                  noteids: step.noteids,
+                  mode: step.mode,
+                  fretnums: step.fretnums
+                }, i)}}
+              >
+                {step.title}
+              </div>
+            )
+        })}
       </div>
     </div>
   )
