d1f2e1e7a69e8cfd4a2e62076392189f7f2898fb
Cam <cameron.miller294@gmail.com>
2022-11-03 23:17:44 -0400

    Update library.tsx
    Added styling to library

--- src/components/library/library.tsx
+++ src/components/library/library.tsx
@@ -5,15 +5,21 @@ import { useAppSelector } from '../../hooks';
 import { libraryChords } from '../../slices/library-slice';
 
 export const Library: React.FC = (): React.ReactElement => {
-  const theChords = useAppSelector(libraryChords);
-  const [chords, setChords] = useState(theChords);
+  const chords = useAppSelector(libraryChords);
+  // const [chords, setChords] = useState(theChords);
 
   return (
     <div className="library">
       <h3 className="section-title">Library</h3>
-      {chords.map(chord => {
-        return <div>{chord.name}</div>
-      })}
+      <div className="lib-chord-wrapper">
+        {chords.map(chord => {
+          return (
+            <div className="lib-chord">
+              {chord.name}
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
