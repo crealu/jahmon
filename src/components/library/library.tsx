060276ad4a4ef57c5a6495c0d863d3c9e8be52f5
Cam <cameron.miller294@gmail.com>
2022-11-03 23:24:41 -0400

    Update library.tsx
    Added click handler to place notes on fretboard

--- src/components/library/library.tsx
+++ src/components/library/library.tsx
@@ -3,10 +3,28 @@ import './library.css';
 import { useState, useEffect } from 'react';
 import { useAppSelector } from '../../hooks';
 import { libraryChords } from '../../slices/library-slice';
+import { clearFretboard } from '../../common/handlers';
 
 export const Library: React.FC = (): React.ReactElement => {
   const chords = useAppSelector(libraryChords);
   // const [chords, setChords] = useState(theChords);
+
+  const placeNotes = (event) => {
+    clearFretboard();
+    const noteIds = event.target.dataset.noteids.split(',');
+    const fretNotes = document.getElementsByClassName('fret-note');
+    for (let n = 0; n < noteIds.length; n++) {
+      for (let fn = 0; fn < fretNotes.length; fn++) {
+        if (noteIds[n] == fretNotes[fn].dataset.noteid) {
+          fretNotes[fn].style.display = 'block';
+          // if (mode == 'riff') {
+          //   const fretnums = step.dataset.fretnums.split(',');
+          //   const trueFretnums = fretnums.filter((fretnum) => fretnum != '');
+          // }
+        }
+      }
+    }
+  }
 
   return (
     <div className="library">
@@ -14,7 +32,11 @@ export const Library: React.FC = (): React.ReactElement => {
       <div className="lib-chord-wrapper">
         {chords.map(chord => {
           return (
-            <div className="lib-chord">
+            <div
+              className="lib-chord"
+              data-noteids={chord.noteids.join(',')}
+              onClick={(e) => placeNotes(e)}
+            >
               {chord.name}
             </div>
           )
