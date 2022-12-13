21535e9eb61020e608af7a774f2cad99899d7c32
Cam <cameron.miller294@gmail.com>
2022-12-13 14:09:30 -0500

    Restructured files

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,16 @@
 import * as React from 'react';
 import './app.css';
-import Sequence from './sequence/sequence';
-import Fretboard from './fretboard/fretboard';
-import Eye from './fretboard/eye';
-import Lyrics from './lyrics/lyrics';
-import Library from './library/library';
-import SequencesDB from './seqdb/seqdb';
-import Save from './save/save';
-import Settings from './settings/settings';
-import Print from './print/print';
-import { useAppSelector } from '../hooks';
-import { isPrinting } from '../slices/view-slice';
+import Sequence from './components/sequence/sequence';
+import Fretboard from './components/fretboard/fretboard';
+import Eye from './components/fretboard/eye';
+import Lyrics from './components/lyrics/lyrics';
+import Library from './components/library/library';
+import SequencesDB from './components/seqdb/seqdb';
+import Save from './components/save/save';
+import Settings from './components/settings/settings';
+import Print from './components/print/print';
+import { useAppSelector } from './hooks';
+import { isPrinting } from './slices/view-slice';
 
 export const App: React.FC = () => {
   const printing = useAppSelector(isPrinting);
