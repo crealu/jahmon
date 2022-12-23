1b909efa62d4a990a1ff104ad76c556c3ed26b27
Cam <cameron.miller294@gmail.com>
2022-12-23 11:44:27 -0500

    Update menu transition
    - Added a smooth JavaScript transition when switching menus between Songs and Library

--- src/components/menu/menuselect.tsx
+++ src/components/menu/menuselect.tsx
@@ -4,13 +4,35 @@ import { useMemo, useCallback } from 'react';
 import { useDispatch } from 'react-redux';
 import { AppDispatch } from '../../store';
 import { useAppSelector } from '../../hooks';
-import { setCurrentMenu } from '../../slices/view-slice';
+import { setCurrentMenu, theMenu } from '../../slices/view-slice';
 
 export const MenuSelect: React.FC = () => {
   const dispatch = useDispatch<AppDispatch>();
+  const menu = useAppSelector(theMenu);
+
+  const animateChange = (event) => {
+    const title = document.getElementsByClassName('menu-title')[0];
+    const content = document.getElementsByClassName('menu-content')[0];
+    let i = 0;
+    let id = setInterval(frame, 10);
+    function frame() {
+      i++;
+      if (i == 25) {
+        title.style.opacity = '0';
+        content.style.opacity = '0'
+      } else if (i == 75) {
+        dispatch(setCurrentMenu(event.target.alt));
+        title.style.opacity = '1';
+        content.style.opacity = '1';
+        clearInterval(id);
+      }
+    }
+  }
 
   const changeMenu = (event) => {
-    dispatch(setCurrentMenu(event.target.alt));
+    if (menu != event.target.alt) {
+      animateChange(event);
+    }
   }
 
   const options = useMemo(() => {
