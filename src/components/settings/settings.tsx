bdf4556227fb39ee242840ed6d829d757cc85157
Cam <cameron.miller294@gmail.com>
2023-01-10 23:57:26 -0500

    Update settings
    - Refactored settings component to conditionally render contents based on state

--- src/components/settings/settings.tsx
+++ src/components/settings/settings.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import { useDispatch } from 'react-redux';
 import { AppDispatch } from '../../../store';
 import { useAppSelector } from '../../hooks';
@@ -7,7 +7,7 @@ import { isSettings, toggleSettings } from '../../slices/view-slice';
 import { lyricLines } from '../../slices/lyrics-slice';
 import { setSteps } from '../../slices/sequence-slice';
 import { theSongTitle, setSongs } from '../../slices/song-slice';
-import { refresh } from '../../common/helpers';
+// import { refresh } from '../../common/helpers';
 import axios from 'axios';
 import './settings.css';
 
@@ -15,9 +15,10 @@ export const Settings: React.FC = () => {
   const dispatch = useDispatch<AppDispatch>();
   const settings = useAppSelector(isSettings);
   const title = useAppSelector(theSongTitle);
-  const closeModal = () => { dispatch(toggleSettings(false)) };
   const [username, setUsername] = useState('');
   const [deleteResponse, setDeleteResponse] = useState('');
+
+  const closeModal = () => { dispatch(toggleSettings(false)) };
 
   const getUsername = () => {
     axios.get('/api-get-username')
@@ -25,7 +26,7 @@ export const Settings: React.FC = () => {
       .catch(err => { throw err });
   }
 
-  const deleteSeq = () => {
+  const deleteSong = () => {
     axios.post('/api-delete-seq', { title: title })
       .then(res => {
         setDeleteResponse(res.data)
@@ -45,15 +46,41 @@ export const Settings: React.FC = () => {
       .catch(err => { throw err });
   }
 
+  const deleteSection = useMemo(() => {
+    return (
+      <div className="delete-wrapper">
+        <button
+          className="delete-song-btn settings-btn modal-btn"
+          onClick={() => deleteSong()}
+        >
+          Delete
+        </button>
+        <p className="song-name">{title}</p>
+      </div>
+    )
+  }, [title]);
+
   useEffect(() => { getUsername() }, []);
 
   return (
     <div className="settings modal" style={{display: settings ? 'block' : 'none'}}>
+      <div className="settings-top">
+        <h2 className="modal-title">Settings</h2>
+        <button
+          className="close-settings-btn"
+          onClick={() => closeModal()}
+        >
+          x
+        </button>
+      </div>
       <h3 className="username">{username}</h3>
-      <a className="logout-btn" href="/logout">Logout</a>
-      <p>Response: {deleteResponse}</p>
-      <button className="delete-seq" onClick={() => deleteSeq()}>Delete Seq</button>
-      <button className="close-settings-btn modal-btn" onClick={() => closeModal()}>Close</button>
+      { title != '' && deleteSection }
+      { username != '' &&
+        <a className="logout-btn" href="/logout">Logout</a>
+      }
+      { deleteResponse != '' &&
+        <p className="delete-response">Response: {deleteResponse}</p>
+      }
   </div>
   )
 }
