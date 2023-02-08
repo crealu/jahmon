1f748140f32826ce290ecb5a499ea332b3c86adb
crealu <cameron.miller294@gmail.com>
2023-02-07 21:13:43 -0500

    Update Carousel.tsx

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,44 +1,53 @@
 import * as React from 'react';
-import './Carousel.tsx';
+import './Carousel.css';
+import { useState, useEffect } from 'react';
 import { useAppSelector } from '../../hooks';
 import { useDispatch } from 'react-redux';
 import { AppDispatch } from '../../store';
-import SeqStep from '../Sequence/SeqStep/SeqStep';
 import { theActiveStep, setActiveStep, setStepName, theStepName, theSteps, addStep } from '../../slices/sequence-slice';
+import Diagram from '../Diagram/Diagram';
 
 export const Carousel: React.FC = () => {
 	const dispatch = useDispatch<AppDispatch>();
 	const steps = useAppSelector(theSteps);
-	const activeStep = useAppSelector(theActiveStep)
+	const [slideNum, setSlideNum] = useState(0);
+
+	const leftArrow = '<';
+	const rightArrow = '>';
 
 	const slide = (direction) => {
-		if (direction == 'left' && activeStep != 0) {
-			dispatch(setActiveStep(activeStep - 1));
+		if (direction == 'left' && slideNum != 0) {
+			setSlideNum(slideNum - 1);
 		}		
-		if (direction == 'right' && activeStep != steps.length - 1) {
-			dispatch(setActiveStep(activeStep + 1));
+		if (direction == 'right' && slideNum != steps.length - 1) {
+			setSlideNum(slideNum + 1);
+		}
+		if (direction == 'right' && slideNum == steps.length - 1) {
+			setSlideNum(0);
+		}
+		if (direction == 'left' && slideNum == 0) {
+			setSlideNum(steps.length - 1);
 		}
 	}
 
-	// const returnSlide = () => {
-	// 	return 
-	// }
-
-	// const returnSteps = useCallback(() => {
-  //   return steps.map((step, i) => { 
-  //   	return (
-  //   		<div>
-
-  //   		</div>
-  //   		<SeqStep step={step} idx={i} key={i} /> 
-  //   })
-  // }, [steps]);
+	const renderSegment = (idx) => {
+		return <div className={`segment ${slideNum == idx ? 'active-segment': ''}`}></div>
+	}
 
 	return (
 		<div className="carousel">
-			<img alt="<" onClick={() => slide('left')}/>
-			<SeqStep step={steps[activeStep]} idx={i} key={i} /> 
-			<img alt=">" onClick={() => slide('right')}/>
+			<div className="caro-top">
+				<div className="progression">
+					{steps.map((step, i) => { return renderSegment(i) })}
+				</div>
+			</div>
+			<div className="caro-inner">	
+				<button className="prev caro-btn" onClick={() => slide('left')}>{leftArrow}</button>
+				<div className="active-slide">
+					<Diagram step={steps[slideNum]} id={0} />
+				</div>
+				<button className="next caro-btn" onClick={() => slide('right')}>{rightArrow}</button>
+			</div>
 		</div>
 	)
 }
