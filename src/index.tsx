4afa8ce245b57169adb9018882091bd4d25cece7
crealu <cameron.miller294@gmail.com>
2023-12-05 17:41:19 -0500

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
 import { Provider } from 'react-redux';
-import store from './store';
 import regeneratorRuntime from 'regenerator-runtime';
+import App from './App';
+import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
